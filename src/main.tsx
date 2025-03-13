87608a8127f6268e63299b9ec5d31dc51a1856bf
m1sh3r <mish.fe2004@gmail.com>
2025-03-13 17:56:06 +0300

    убраны неиспользуемые зависимости

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
 import {App} from "./App";
 import {ThemeProvider} from "./components/modal/ThemeChanger";
-import "swiper/css";
 import {HelmetProvider} from "react-helmet-async";
 import "./main.scss";
 
