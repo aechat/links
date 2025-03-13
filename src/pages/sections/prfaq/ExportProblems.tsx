dabfaa920c2d7391c9e1753eda3158b5b8e5f715
m1sh3r <mish.fe2004@gmail.com>
2025-03-13 20:23:05 +0300

    первичная реализация пункта про ошибку -1609629695

--- src/pages/sections/prfaq/ExportProblems.tsx
+++ src/pages/sections/prfaq/ExportProblems.tsx
@@ -2,11 +2,67 @@ import React from "react";
 import GithubUpdateInfo from "../../../components/features/GithubUpdateInfo";
 import DetailsSummary from "../../../components/DetailsSummary";
 import {ImageFigure} from "../../../components/ContentFigure";
+import {AdditionWarning} from "../../../components/Additions";
 
 const PRExportProblems: React.FC = () => {
   return (
     <div className="faq-content">
       <GithubUpdateInfo filePath="src/pages/sections/prfaq/ExportProblems.tsx" />
+      <DetailsSummary title="Код ошибки: -1609629695 при экспорте">
+        <p>
+          Данная ошибка может возникнуть по совершенно разным причинам. У данной ошибки
+          нет универсального решения, но от неё можно попробовать избавиться, если
+          выполнить некоторые пункты ниже.
+        </p>
+        <AdditionWarning>
+          Приведённые ниже способы{" "}
+          <u>
+            <b>не являются панацеей</b>
+          </u>{" "}
+          для решения и позволяют лишь исключить часть проблем при экспорте.
+        </AdditionWarning>
+        <ul>
+          <li>
+            Попробуйте отключить <mark>GPU-ускорение</mark> в настройках проекта на время.
+            Для этого перейдите в{" "}
+            <mark className="ui">File &gt; Project Settings &gt; General</mark> и выберите
+            в пункте <mark className="ui">Renderer</mark> значение{" "}
+            <mark className="ui">Mercury Playback Engine Software Only</mark>.
+            <AdditionWarning>
+              Скорость экспорта при переключении рендеринга на процессор может измениться
+              в лучшую или худшую сторону.
+            </AdditionWarning>
+          </li>
+          <li>
+            Убедитесь в том, что вы не используете кириллицу в путях исходников, а также в
+            пути для вывода файла. Программы от Adobe с давних пор не любят кириллицу в
+            путях и эта проблема тянется и до последних версий.
+          </li>
+          <li>
+            Если вы выводите в формате <mark className="video">H.264</mark> напрямую из{" "}
+            <mark className="app">Premiere Pro</mark> - попробуйте выбрать кодек{" "}
+            <mark className="video">Apple Prores 442</mark> для экспорта. Полученное видео
+            вы можете переконвертировать любым способом, например через{" "}
+            <mark className="app">Shutter Encoder</mark> или{" "}
+            <mark className="app">Handbrake</mark>.
+          </li>
+          <li>
+            Если вы выводите через <mark className="app">Media Encoder</mark>, то
+            откажитесь от него в пользу экспорта напрямую из{" "}
+            <mark className="app">Premiere Pro</mark>.
+          </li>
+          <li>
+            Перед импортом чужих исходников в проект, рекомендуется их перекодировать в
+            монтажный кодек, например в <mark className="video">Apple Prores 422</mark>,
+            чтобы исключить различные проблемы при экспорте.
+          </li>
+          <li>
+            При экспорте вы можете попробовать использовать <mark>Previews</mark>, если
+            приведённые выше способы не помогают.
+          </li>
+        </ul>
+        {/* TODO: написать при возможности ещё варианты решения */}
+      </DetailsSummary>
       <DetailsSummary title="Почему созданные субтитры не отображаются на экспортированной секвенции?">
         <p>
           По умолчанию в <mark className="app">Premiere Pro</mark> отключен параметр{" "}
