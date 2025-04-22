c5f3205886f183aa69a6e068e034f6f5b590a754
m1sh3r <mish.fe2004@gmail.com>
2025-04-22 15:32:02 +0300

    поправили функцию копирования кода

--- src/components/features/CodeSnippet.tsx
+++ src/components/features/CodeSnippet.tsx
@@ -1,12 +1,14 @@
 import React, {useEffect, useRef} from "react";
 import hljs from "highlight.js";
-import "highlight.js/styles/github-dark-dimmed.css";
+import "highlight.js/styles/github-dark.css";
+import {message} from "antd";
+
 interface CodeSnippetProps {
-  language: string;
+  language?: string;
   children: string;
 }
 
-const CodeSnippet: React.FC<CodeSnippetProps> = ({language, children}) => {
+const CodeSnippet: React.FC<CodeSnippetProps> = ({language = "javascript", children}) => {
   const codeRef = useRef<HTMLElement | null>(null);
   useEffect(() => {
     if (codeRef.current) {
@@ -14,11 +16,26 @@ const CodeSnippet: React.FC<CodeSnippetProps> = ({language, children}) => {
     }
   }, [children, language]);
 
+  const handleCopy = (event: React.MouseEvent<HTMLPreElement>) => {
+    event.stopPropagation();
+
+    const textArea = document.createElement("textarea");
+
+    const codeText = codeRef.current?.textContent ?? "";
+    textArea.value = codeText;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    textArea.remove();
+    message.success("Код скопирован в буфер обмена");
+  };
+
   return (
-    <pre>
+    <pre onClick={handleCopy}>
       <code
         ref={codeRef}
         className={language}
+        style={{userSelect: "none"}}
       >
         {children}
       </code>
