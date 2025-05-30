641fba7604e2cee0de9ab3c0d1d1ffbfcf6edbb1
m1sh3r <mish.fe2004@gmail.com>
2025-05-30 22:55:45 +0300

    рефакторинг компонента поиска по странице

---
+++ src/components/search/index.tsx
@@ -0,0 +1,4 @@
+export {SearchProvider, useSearch} from "./context";
+export {SearchButton} from "./components/SearchButton";
+export {WideSearchButton} from "./components/WideSearchButton";
+export {SearchInPage} from "./components/SearchInPage";
