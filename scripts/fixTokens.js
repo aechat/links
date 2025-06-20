511e8ecb550ec26dab49c72369e858edadf97ff0
m1sh3r <mish.fe2004@gmail.com>
2025-06-20 13:47:29 +0300

    перенос скриптов в отдельную директорию
    (cherry picked from commit 9900d4275b795f46ca412aff52ead755b96e8519)

---
+++ scripts/fixTokens.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+import {readFileSync, readdirSync, statSync, writeFileSync} from "fs";
+import {join} from "path";
+import {cwd} from "process";
+
+function fixFileTags(content) {
+  return content.replace(/("file">)\.(.*?)(<)/g, (_, open, word, close) => {
+    return `${open}${word.toUpperCase()}${close}`;
+  });
+}
+
+function fixKeyTags(content) {
+  return content.replace(/("key">)(.*?)(<)/g, (_, open, keyContent, close) => {
+    const fixed = keyContent.replace(/\s*\+\s*/g, " + ");
+
+    return `${open}${fixed}${close}`;
+  });
+}
+
+function walkAndFix(dir) {
+  const files = readdirSync(dir);
+  for (const file of files) {
+    const fullPath = join(dir, file);
+
+    const stat = statSync(fullPath);
+
+    if (stat.isDirectory()) {
+      walkAndFix(fullPath);
+    } else if (file.endsWith(".tsx")) {
+      const original = readFileSync(fullPath, "utf8");
+
+      const fixed = fixKeyTags(fixFileTags(original));
+
+      if (original !== fixed) {
+        writeFileSync(fullPath, fixed);
+        console.log(`✔ updated: ${fullPath}`);
+      }
+    }
+  }
+}
+
+walkAndFix(cwd());
