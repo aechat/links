fe384c4689d22cfa316e9e20597599801c097829
m1sh3r <mish.fe2004@gmail.com>
2025-08-16 22:02:05 +0300

    временно убрано отображение последних изменений

--- src/components/features/GithubUpdateInfo.tsx
+++ src/components/features/GithubUpdateInfo.tsx
@@ -1,102 +1,10 @@
-import React, {useEffect, useState} from "react";
-
-const OWNER = "aechat";
-
-const REPO = "links";
-
-const BRANCH = "main";
-
-const isWebKit =
-  typeof navigator !== "undefined" &&
-  /AppleWebKit|Epiphany|Safari/i.test(navigator.userAgent) &&
-  !/Chrome|Chromium|Edg|OPR|Brave/i.test(navigator.userAgent);
+import React from "react";
 
 interface GithubUpdateInfoProps {
   filePath: string;
 }
 
-const GithubUpdateInfo: React.FC<GithubUpdateInfoProps> = ({filePath}) => {
-  const [commitInfo, setCommitInfo] = useState<string>("Ищем информацию...");
-
-  const getLastCommitDate = async () => {
-    const url = `https://api.github.com/repos/${OWNER}/${REPO}/commits?path=${filePath}&sha=${BRANCH}`;
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`${response.status}`);
-      }
-
-      const commits = await response.json();
-      interface Commit {
-        commit: {
-          message: string;
-        };
-      }
-
-      const filteredCommits = commits.filter(
-        (commit: Commit) => !commit.commit.message.startsWith("Merge")
-      );
-      if (filteredCommits.length === 0) {
-        setCommitInfo("Индикатор свежести информации временно недоступен");
-
-        return;
-      }
-
-      const lastCommitDate = new Date(filteredCommits[0].commit.author.date);
-
-      const dateOptions: Intl.DateTimeFormatOptions = {
-        year: "2-digit",
-        month: "2-digit",
-        day: "2-digit",
-      };
-
-      const clockOptions: Intl.DateTimeFormatOptions = {
-        hour: "2-digit",
-        minute: "2-digit",
-      };
-
-      const formattedDate = lastCommitDate.toLocaleString("ru-RU", dateOptions);
-
-      const formattedClock = lastCommitDate.toLocaleString("ru-RU", clockOptions);
-
-      const commitMessage =
-        filteredCommits[0].commit.message.charAt(0).toLowerCase() +
-        filteredCommits[0].commit.message.slice(1);
-
-      const commitUrl = filteredCommits[0].html_url;
-      setCommitInfo(
-        `Обновлено ${formattedDate} в ${formattedClock}: <a style="font-weight: 800; lineHeight: "1.35"" target="_blank" rel="noreferrer" href="${commitUrl}" target="_blank">${commitMessage}</a>`
-      );
-    } catch (err) {
-      setCommitInfo(`Индикатор свежести информации временно недоступен - ${err}`);
-    }
-  };
-  useEffect(() => {
-    getLastCommitDate();
-  }, [filePath]);
-
-  if (isWebKit) {
-    return null;
-  }
-
-  return (
-    <p
-      dangerouslySetInnerHTML={{__html: commitInfo}}
-      style={{
-        filter: "saturate(0.5)",
-        fontSize: "0.75rem",
-        opacity: "0.5",
-        height: "20px",
-        textAlign: "right",
-        overflow: "hidden",
-        textOverflow: "ellipsis",
-        whiteSpace: "nowrap",
-        fontWeight: "400",
-        lineHeight: "1.35",
-        marginBlockEnd: "20px",
-        marginInline: "10px",
-      }}
-    />
-  );
+const GithubUpdateInfo: React.FC<GithubUpdateInfoProps> = () => {
+  return null;
 };
 export default GithubUpdateInfo;
