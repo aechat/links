0f730a7ce7e38361c2112af5d359a24ec3d4c82a
m1sh3r <mish.fe2004@gmail.com>
2025-09-06 22:16:56 +0300

    убраны иконки

--- src/components/Addition.tsx
+++ src/components/Addition.tsx
@@ -1,5 +1,3 @@
-import {InfoOutlined, ReportOutlined, WarningAmberOutlined} from "@mui/icons-material";
-
 import React, {ReactNode} from "react";
 
 interface AdditionProps {
@@ -8,36 +6,31 @@ interface AdditionProps {
 }
 
 const Addition: React.FC<AdditionProps> = ({children, type}) => {
-  let icon: ReactNode;
   let className: string;
 
   switch (type) {
     case "info":
-      icon = <InfoOutlined />;
       className = "addition-info";
 
       break;
 
     case "warning":
-      icon = <WarningAmberOutlined />;
       className = "addition-warning";
 
       break;
 
     case "danger":
-      icon = <ReportOutlined />;
       className = "addition-danger";
 
       break;
   }
 
-  if (!icon || !className) {
+  if (!className) {
     throw new Error("Не указан тип для компонента Addition");
   }
 
   return (
     <div className={className}>
-      {icon}
       <div>{children}</div>
     </div>
   );
