435053200cc832f0e8b6d57677257f6a59c141f4
m1sh3r <mish.fe2004@gmail.com>
2026-01-27 15:33:25 +0300

    refactor(components): убран функционал увеличения изображения для дальнейшего рефакторинга

--- src/components/content/ArticleMedia.tsx
+++ src/components/content/ArticleMedia.tsx
@@ -1,9 +1,8 @@
-import React, {useCallback, useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import mediaMetadata from "virtual:media-metadata";
 
 import {ShareRounded} from "@mui/icons-material";
 import {message} from "antd";
-import {createPortal} from "react-dom";
 
 import {copyText} from "../../hooks/useCopyToClipboard";
 
@@ -82,77 +81,11 @@ const ArticleMedia: React.FC<ArticleMediaProperties> = (properties) => {
 
   const [hasBeenOpened, setHasBeenOpened] = useState(isSpoilerOpen);
 
-  const [isFullscreen, setIsFullscreen] = useState(false);
-
-  const [isClosing, setIsClosing] = useState(false);
-
-  const [initialScrollY, setInitialScrollY] = useState(0);
-
   useEffect(() => {
     if (isSpoilerOpen) {
       setHasBeenOpened(true);
     }
   }, [isSpoilerOpen]);
-
-  const handleMaximize = useCallback(() => {
-    setInitialScrollY(window.scrollY);
-    setIsFullscreen(true);
-    setIsClosing(false);
-  }, []);
-
-  const handleClose = useCallback(() => {
-    setIsClosing(true);
-
-    setTimeout(() => {
-      setIsFullscreen(false);
-      setIsClosing(false);
-    }, 250);
-  }, []);
-
-  const handleClick = useCallback(() => {
-    if (isFullscreen) {
-      handleClose();
-    } else {
-      handleMaximize();
-    }
-  }, [isFullscreen, handleClose, handleMaximize]);
-
-  const handleClickOutside = useCallback(
-    (event: React.MouseEvent<HTMLDivElement>) => {
-      if (event.target === event.currentTarget) {
-        handleClose();
-      }
-    },
-    [handleClose]
-  );
-
-  useEffect(() => {
-    const handleEscKey = (event: KeyboardEvent) => {
-      if (event.key === "Escape" && isFullscreen) {
-        handleClose();
-      }
-    };
-
-    if (isFullscreen) {
-      document.addEventListener("keydown", handleEscKey);
-    }
-
-    return () => document.removeEventListener("keydown", handleEscKey);
-  }, [isFullscreen, handleClose]);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (Math.abs(window.scrollY - initialScrollY) > window.innerHeight * 0.25) {
-        handleClose();
-      }
-    };
-
-    if (isFullscreen) {
-      window.addEventListener("scroll", handleScroll);
-    }
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [isFullscreen, handleClose, initialScrollY]);
 
   const renderCaption = () => {
     if (!caption || type === "youtube") {
@@ -189,7 +122,6 @@ const ArticleMedia: React.FC<ArticleMediaProperties> = (properties) => {
             src={resolvedSource}
             style={{aspectRatio}}
             width={metadata?.width}
-            onClick={handleClick}
           />
         );
       }
@@ -267,21 +199,6 @@ const ArticleMedia: React.FC<ArticleMediaProperties> = (properties) => {
           {renderCaption()}
         </figure>
       </div>
-      {isFullscreen &&
-        createPortal(
-          <div
-            className={`${styles["media-fullscreen-portal"]} ${
-              isClosing ? styles["media-fullscreen-portal--closing"] : ""
-            }`}
-            onClick={handleClickOutside}
-          >
-            <div className={`${styles["media-fullscreen-content"]} ${figureClass}`}>
-              {renderContent()}
-              {renderCaption()}
-            </div>
-          </div>,
-          document.body
-        )}
     </>
   );
 };
