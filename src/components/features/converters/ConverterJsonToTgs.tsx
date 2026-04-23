3e899909ce90c9a07a0a5b1717678eaa990256bf
m1sh3r <mish.fe2004@gmail.com>
2026-04-23 20:12:57 +0300

    refactor(components): переделаны кнопки скачивания и сброса файла у конвертеров

--- src/components/features/converters/ConverterJsonToTgs.tsx
+++ src/components/features/converters/ConverterJsonToTgs.tsx
@@ -215,15 +215,15 @@ const JsonToTgsConverter: React.FC = () => {
         )}
       </p>
       {jsonData && typeof jsonData === "object" && (
-        <div className={styles["converter-button-group"]}>
+        <div className={`${modalStyles["modal-controls-group"]}`}>
           <button
-            className={`${modalStyles["modal-open-button"]} ${styles["converter-button-reset"]}`}
+            className={`${modalStyles["modal-controls-option"]} ${styles["converter-reset-desaturated"]}`}
             onClick={handleReset}
           >
             Сбросить
           </button>
           <button
-            className={`${modalStyles["modal-open-button"]} ${styles["converter-button-action"]}`}
+            className={`${modalStyles["modal-controls-option"]} ${styles["converter-button-action"]}`}
             disabled={isLoading}
             onClick={handleDownload}
           >
